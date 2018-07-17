e225a1061e517c173c2a7b1201ae30d63f094ff7
Peter Olayinka <peterolayinka97@gmail.com>
2018-07-17 14:55:21 +0100

    update methods and requirement file

--- views.py
+++ views.py
@@ -7,25 +7,26 @@ subject=SubjectPricing()
 
 def get_state_factor(request, state='lagos'):
     data=subject.get_state_factor(state)
-    return  JsonResponse({'fator':data})
+    return  JsonResponse({data})
 
 def get_all_states_with_vicinities(request):
     data=subject.get_all_states_with_vicinities()
-    return  JsonResponse({'data':data})
+    return  JsonResponse({data})
 
 def get_state_vicinities(request, state='lagos'):
     data=subject.get_state_vicinities(state)
-    return  JsonResponse({'data':data})
+    return  JsonResponse({data})
 
 def get_vicinity_factor(request, vicinity='lagos'):
     data=subject.get_vicinity_factor(vicinity)
-    return JsonResponse({"vicinity":data})
+    return JsonResponse({data})
 
 def get_all_purposes_and_factors(request):
     data=subject.get_all_purposes_and_factors()
-    return  JsonResponse({'data':data})
+    return  JsonResponse({data})
 
 def get_purpose_factor(request, purpose=[]):
-    new_purpose = purpose.strip().split(',')
+    new_purpose = purpose.strip().\
+                    replace('-', ' ').split(',')
     data=subject.get_purpose_factor(new_purpose)
-    return  JsonResponse({'data':data})
+    return  JsonResponse({data})
