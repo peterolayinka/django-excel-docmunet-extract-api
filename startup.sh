1dc65bd2c88615b35b97d1414a1ce93ef32181ef
Peter Olayinka <peterolayinka97@gmail.com>
2018-07-17 13:14:41 +0100

    added neccessary files

--- startup.sh
+++ startup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
 source venv/bin/activate
 
-if [ $1 == true ] 
+if [[ $1 == true ]]
 then 
     python manage.py runserver 0.0.0.0:4400
+elif [[ $1 == "mg" ]]
+then 
+    python manage.py migrate
+elif [[ $1 == "mkmg" ]]
+then 
+    python manage.py makemigrations
 fi
